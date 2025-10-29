cca0560e01d6634917fb60a44742ccb9479b2fd9
Oleh Grypas <olehgrypas@gmail.com>
2025-10-29 18:21:17 +0100

    added some basic config for the port the website will use

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,15 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	
+    port := os.Getenv("PORT")
+	fmt.Println("Attempting to start the server on port", port)
+    if port == "" {
+        port = "8080"
+    }
+
+
 	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting the server on :3000")
+	fmt.Println("Starting the server on port", port)
 	http.ListenAndServe(":8080", nil)
 }
